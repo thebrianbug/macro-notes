4821cb70265b66ac8df0265c79a3c2661c758c6c
Brian McIlwain <technicallyemployed@gmail.com>
2021-03-03 18:11:43 -0600

    Fixed more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,31 @@ module.exports = {
     },
     extend: {
       colors: {},
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.100'),
+            h1: {
+              color: theme('colors.gray.200')
+            },
+            h2: {
+              color: theme('colors.gray.200')
+            },
+            h3: {
+              color: theme('colors.gray.200')
+            },
+            h4: {
+              color: theme('colors.gray.200')
+            },
+            'ol > li::before': {
+              color: theme('colors.gray.300'),
+            },
+            'ul > li::before': {
+              backgroundColor: theme('colors.gray.300'),
+            },
+          },
+        },
+      }),
     },
   },
   variants: {},
