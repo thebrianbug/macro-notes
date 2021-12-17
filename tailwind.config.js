2e97830688e2faf485ae45f5994d049a8a9a3563
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-16 21:33:38 -0600

    Ran prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,27 +15,27 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.gray.100'),
+            color: theme("colors.gray.100"),
             h1: {
-              color: theme('colors.gray.200')
+              color: theme("colors.gray.200"),
             },
             h2: {
-              color: theme('colors.gray.200')
+              color: theme("colors.gray.200"),
             },
             h3: {
-              color: theme('colors.gray.200')
+              color: theme("colors.gray.200"),
             },
             h4: {
-              color: theme('colors.gray.200')
+              color: theme("colors.gray.200"),
             },
-            'ol > li::before': {
-              color: theme('colors.gray.300'),
+            "ol > li::before": {
+              color: theme("colors.gray.300"),
             },
-            'ul > li::before': {
-              backgroundColor: theme('colors.gray.300'),
+            "ul > li::before": {
+              backgroundColor: theme("colors.gray.300"),
             },
             a: {
-              color: theme('colors.purple.400'),
+              color: theme("colors.purple.400"),
             },
           },
         },
